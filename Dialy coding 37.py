d2c270fbe4c882cd5b7374867b634ce35abf2e22
Saiteja Nannaka <98960483+SaitejaNannaka@users.noreply.github.com>
2022-06-10 13:07:03 +0530

    Update Dialy coding 37.py

--- Dialy coding 37.py
+++ Dialy coding 37.py
@@ -5,46 +5,15 @@ Example:
 
 Set  = [a,b,c]
 power_set_size = pow(2, 3) = 8
-Run for binary counter = 000 to 111
-
-Value of Counter            Subset
-   000                    -> Empty set
-   001                    -> a
-   010                    -> b
-   011                    -> ab
-   100                    -> c
-   101                    -> ac
-   110                    -> bc
-   111                    -> abc
 
 
 
-### python3 program for power set
-
-import math;
-
-def printPowerSet(set,set_size):
-	
-	# set_size of power set of a set
-	# with set_size n is (2**n -1)
-	pow_set_size = (int) (math.pow(2, set_size));
-	counter = 0;
-	j = 0;
-	
-	# Run from counter 000..0 to 111..1
-	for counter in range(0, pow_set_size):
-		for j in range(0, set_size):
-			
-			# Check if jth bit in the
-			# counter is set If set then
-			# print jth element from set
-			if((counter & (1 << j)) > 0):
-				print(set[j], end = "");
-		print("");
-
-# Driver program to test printPowerSet
-set = ['a', 'b', 'c'];
-printPowerSet(set, 3);
-
-# This code is contributed by mits.
+#Python program to find powerset
+from itertools import combinations
+def print_powerset(string):
+	for i in range(0,len(string)+1):
+		for element in combinations(string,i):
+			print(''.join(element))
+string=['a','b','c']
+print_powerset(string)
 
